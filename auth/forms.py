f4cb3fba0df11f49e0fed4d39a6bd0c6f20e85f4
ssengup <ssengup@gmail>
2022-01-19 21:33:34 +0530

    added the all the post method for signup and now button are actiove and now we can save the values and proceed

--- auth/forms.py
+++ auth/forms.py
@@ -8,7 +8,7 @@ class Signupform(UserCreationForm):
     firstname = forms.CharField(max_length=100)
     lastname = forms.CharField(max_length=100)
     email = forms.EmailField(max_length=100)
-    photo = forms.FileField(allow_empty_file=True)
+    #photo = forms.FileField(allow_empty_file=True)
     class Meta :
         model = User
-        fields = ('firstname' , 'lastname' ,'username' , 'email' , 'password1' , 'password2' , 'photo')
+        fields = ('firstname' , 'lastname' ,'username' , 'email' , 'password1' , 'password2' )
--- auth/views.py
+++ auth/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.views import LoginView , LogoutView
 from django.views import View
 from django.contrib.auth.forms import UserCreationForm  # we are not using basic one we are updating that 
 from auth import forms
+from django.contrib.auth import login
 # Create your views here.
 
 
@@ -20,4 +21,15 @@ class Signup(View):
             'form' :forms.Signupform()
         }
         return render(request , 'auth/signup.html' , context)
+    def post(self ,request):
+        form = forms.Signupform(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request , user)
+            return redirect('/')
+
+        context = {
+            'form' : form
+        }
+        return render(request , 'auth/signup.html' ,context)
     
